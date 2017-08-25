8508f68e99f11b04daf35e08f15e051ac6bfbce0
John Brewer <jbrewer@jera.com>
2017-08-24 23:21:32 -0700

    Actually works when source == dest.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
-constexpr int size = 4;
+constexpr int size = 5;
 
 static void RotateBitmap90DegreesClockwise(const uint8_t *source, uint8_t *dest) {
-    for (int i = 0; i < size / 2 + 1; i++) {
-        for (int j = 0; j < size / 2 + 1; j++) {
-            uint8_t save = source[j * size + i];
-            dest[j * size + i] = source[(size - i - 1) * size + j];
-            dest[(size - i - 1) * size + j] = source[(size - i - 1) * size + size - j - 1];
-            dest[(size - i - 1) * size + size - j - 1] = source[i * size + size - j - 1];
-            dest[i * size + size - j - 1] = save;
+    for (int i = 0; i < size / 2 + size % 2; i++) {
+        for (int j = i; j < size - 1 - i; j++) {
+            int iprime = size - 1 - i;
+            int jprime = size - 1 - j;
+            uint8_t save = source[i * size + j];
+            dest[i * size + j] = source[jprime * size + i];
+            dest[jprime * size + i] = source[iprime * size + jprime];
+            dest[iprime * size + jprime] = source[j * size + iprime];
+            dest[j * size + iprime] = save;
         }
     }
 }
