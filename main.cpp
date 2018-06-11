0c5f1952967772f02cbd808d5321abd67dfe13a0
John Brewer <jbrewer@jera.com>
2018-06-10 22:48:03 -0700

    Fixed non-in-place solution for odd SIZE.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,17 @@ constexpr int SIZE = 5;
 // uvwxy     ytoje
 //
 static void RotateImage90DegreesClockwise(const uint8_t *source, uint8_t *dest) {
+    for (int i = 0; i < SIZE / 2 + SIZE % 2; i++) {
+        for (int j = i; j < SIZE - i; j++) {
+            int iprime = SIZE - 1 - i;
+            int jprime = SIZE - 1 - j;
+            uint8_t save = source[i * SIZE + j];
+            dest[i * SIZE + j] = source[jprime * SIZE + i];
+            dest[jprime * SIZE + i] = source[iprime * SIZE + jprime];
+            dest[iprime * SIZE + jprime] = source[j * SIZE + iprime];
+            dest[j * SIZE + iprime] = save;
+        }
+    }
 }
 
 int main() {
