5275bf4cfe2a09b7dd8128a7722516462312c0a9
John Brewer <jbrewer@jera.com>
2018-06-10 22:46:35 -0700

    Clean up file; add test.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstring>
+
+using namespace std;
 
 constexpr int SIZE = 5;
 
@@ -11,24 +14,20 @@ constexpr int SIZE = 5;
 // uvwxy     ytoje
 //
 static void RotateImage90DegreesClockwise(const uint8_t *source, uint8_t *dest) {
-    for (int i = 0; i < SIZE / 2 + SIZE % 2; i++) {
-        for (int j = i; j < SIZE - 1 - i; j++) {
-            int iprime = SIZE - 1 - i;
-            int jprime = SIZE - 1 - j;
-            uint8_t save = source[i * SIZE + j];
-            dest[i * SIZE + j] = source[jprime * SIZE + i];
-            dest[jprime * SIZE + i] = source[iprime * SIZE + jprime];
-            dest[iprime * SIZE + jprime] = source[j * SIZE + iprime];
-            dest[j * SIZE + iprime] = save;
-        }
-    }
 }
 
 int main() {
     char source[26] = "abcdefghijklmnopqrstuvwxy";
     char dest[26]   = "                         ";
+    char target[26] = "upkfavqlgbwrmhcxsnidytoje";
     RotateImage90DegreesClockwise((uint8_t *)source, (uint8_t *)dest);
     dest[25] = 0;
-    std::cout << dest << std::endl;
+    cout << "target: " << target << endl;
+    cout << "dest:   " << dest << endl;
+    if (!strncmp(dest, target, SIZE * SIZE)) {
+        cout << "Correct." << endl;
+    } else {
+        cout << "Incorrect." << endl;
+    }
     return 0;
 }
